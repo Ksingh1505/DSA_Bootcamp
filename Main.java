5fdfa7430d0fd927ce4a63ac6602a0db897273c9
Ksingh1505 <Ksingh.1505004@gmail.com>
2026-03-27 11:00:38 +0530

    optimal approach of floor sqrt

--- Main.java
+++ Main.java
@@ -1,10 +1,33 @@
-class Solution{
-    public int floor_sqrt(int n){
-        int i=1;
-        while(i*i <= n){
-            i++;
+//BRUTE FORCE
+// class Solution{
+//     public int floor_sqrt(int n){
+//         int i=1;
+//         while(i*i <= n){
+//             i++;
+//         }
+//         return i-1;
+//     }
+// }
+
+
+//OPTIMAL
+class Solution {
+    public int floor_sqrt(int n) {
+        int low = 1, high = n;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if ((long) mid * mid <= n) {
+                ans = mid;        // possible answer
+                low = mid + 1;    // try bigger
+            } else {
+                high = mid - 1;   // go smaller
+            }
         }
-        return i-1;
+
+        return ans;
     }
 }
 public class Main {
